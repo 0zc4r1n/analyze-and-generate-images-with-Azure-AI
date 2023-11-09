e68d17b744f9753c4cc5b646473e99dd1966e16c
Oscar Pérez Guerrero <oscarperezguerrero@gmail.com>
2023-11-09 12:35:42 -0300

    ADD Simple GUI

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import React from 'react';
 
 function App() {
-  const value = 'World';
-  return <div>Hello {value}</div>;
+// Create a simple GUI in React with the following elements:
+//    1. A title
+//    2. A text input to write a Image URL to analize
+//    3. A button to analize the image
+//    4. A button to generate the image
+
+  return (
+    <div className="App">
+      <h1>Computer vision</h1>
+      <p>Inser URL or type prompt</p>
+      <input type="text" placeholder="Enter URL to analize" />
+      <button>Analize</button>
+      <button>Generate</button>
+    </div>
+  );
 }
 
 export default App;
